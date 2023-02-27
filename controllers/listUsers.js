1c9d77069dbef8b3eb4f3a02230368b7a6a977a6
Yulia Semenyuk <semenyukyp@gmail.com>
2023-02-27 21:23:43 +0100

    list for admin

--- controllers/listUsers.js
+++ controllers/listUsers.js
@@ -1,5 +1,10 @@
+const { User } = require('../models/userModel');
+
 const listUsers = async (req, res) => {
     console.log('boo');
+    // admin
+    const users = await User.find();
+    res.json(users)
 }
 
 module.exports = listUsers;
