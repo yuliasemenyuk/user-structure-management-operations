c424592396c6e2a3fdaf05a1a73b45dda0a4552a
Yulia Semenyuk <semenyukyp@gmail.com>
2023-02-28 13:03:26 +0100

    list user ctrl

--- controllers/listUsers.js
+++ controllers/listUsers.js
@@ -1,10 +1,34 @@
 const { User } = require('../models/userModel');
 
 const listUsers = async (req, res) => {
-    console.log('boo');
+    const {role} = req.user; 
+    const user = req.user;
+    console.log(role);
+    switch (role) {
+        case 'admin':
+            const users = await User.find({},  '-password -token' );
+            res.json(users)
+          break;
+        case 'regular':
+         
+          res.json(user)
+          break;
+        case 'boss':
+            const {_id: boss} = req.user;
+            console.log(boss);
+            const subordinates = await User.find({boss});
+            console.log(subordinates);
+            res.json(
+                {user,
+                subordinates: subordinates}
+            )
+          break;
+        default:
+          log('default')
+          break;
+      }
     // admin
-    const users = await User.find();
-    res.json(users)
+
 }
 
 module.exports = listUsers;
